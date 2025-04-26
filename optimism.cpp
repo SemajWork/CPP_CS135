41b184333d171e37ef3dfea350da17faacfa5b5a
SemajWork <jamesma765@gmail.com>
2025-04-26 13:19:57 -0400

    idk
    asdf

--- optimism.cpp
+++ optimism.cpp
@@ -15,3 +15,14 @@ std::vector<int> goodVibes(const std::vector<int>& v){
     }
     return posvec;
 }
+
+int main() {
+    std::vector<int> v{1, 2, -1, 3, 4, -1, 6};
+    std::vector<int> hold = goodVibes(v);
+
+    for (int i : hold) {
+        std::cout << i << " ";
+    }
+
+    return 0;
+}
